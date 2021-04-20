63032b04b1402fb1d74355d0b30a0206f6fb72d5
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-04-20 21:44:23 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require("gulp"); 
+const sass = require("gulp-sass");
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+sass.compiler = require('sass')
+
+
+function server(cb) { 
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    cb();
+}
+
+function makeCss() {
+    return gulp.src('./scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "expanded"
+        }))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+}
+
+function watch(cb) {
+    gulp.watch("./scss/**/*.scss", gulp.series(makeCss));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./*.js").on('change', browserSync.reload);
+    cb();
+}
+
+module.exports.makeCss = makeCss;
+module.exports.watch = watch;
+
+module.exports.default = gulp.series(server, makeCss, watch)
